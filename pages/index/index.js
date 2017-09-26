e370a0743990b93a6e92d57bc0a0efd174fe3464
zhangjunTracy <13552960783@163.com>
2017-09-26 21:51:50 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,117 +1,30 @@
+const { requsetForGet } = require('../../utils/request')
+
 Page({
   data: {
-    title: '',
-    movieList: [{ category: '1',
-      title: '近期上映',
-      subjects: [
-        {
-          imgUrls: 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2494093630.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "37",
-            min: 0
-          },
-          title: "猩球崛起3:终极之战"
-        },
-        {
-          imgUrls: 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2498055621.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "40",
-            min: 0
-          },
-          title: "王牌保镖"
-        },
-        {
-          imgUrls: 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2497756471.webp',
-          rating: {
-            max: 10,
-            average: 8.0,
-            stars: "35",
-            min: 0
-          },
-          title: "蜘蛛侠：英雄归来"
-        },
-        {
-          imgUrls:'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2498971355.webp',
-          rating: {
-            max: 10,
-            average: 0,
-            stars: "35",
-            min: 0
-          },
-          title: "看不见的客人"
-        },
-        {
-          imgUrls: 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2498542692.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "35",
-            min: 0
-          },
-          title: "捍卫者"
-        },
-        {
-          imgUrls: 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2499141363.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "35",
-            min: 0
-          },
-          title: "天梯：蔡国强的艺术"
-        },
-        {
-          imgUrls:'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2498371582.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "35",
-            min: 0
-          },
-          title: "刀剑神域：序列之争"
-        },
-        {
-          imgUrls: 'https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2499708853.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "35",
-            min: 0
-          },
-          title: "昆塔：反转星球"
-        },
-        {
-          imgUrls: 'https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2498897918.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "35",
-            min: 0
-          },
-          title: "理查大冒险"
-        },
-        {
-          imgUrls: 'https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2499416137.webp',
-          rating: {
-            max: 10,
-            average: 7.1,
-            stars: "35",
-            min: 0
-          },
-          title: "纯洁心灵·逐梦演艺圈"
-        }
-      ]
-    }],
-
+    title: 'urlDemo',
+    movieList: [
+      { url: 'https://api.douban.com/v2/movie/in_theaters' },
+      { url: 'https://api.douban.com/v2/movie/coming_soon' },
+      { url: 'https://api.douban.com/v2/movie/top250' }
+      //有次数限制 先去掉 其它完成之后加上 加缓存
+      // { url:'https://api.douban.com/v2/movie/us_box'}
+    ],
   },
   //生命周期函数--监听页面加载
   onLoad: function () {
-    this.setData({
-      title: 'Demo'
+    let allPromises = this.data.movieList.map(m => {
+      return requsetForGet(m.url, { count: 10 }).then((res) => {
+        m.title = res.data.title
+        m.subjects = res.data.subjects
+        return m
+      })
+    })
+    Promise.all(allPromises).then((res) => {
+      this.setData({
+        movieList: res
+      })
+      console.info(res)
     })
   }
 })
