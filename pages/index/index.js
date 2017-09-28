48295b59bb7f0a80a382e30f1d995e0f29518605
zhangjunTracy <13552960783@163.com>
2017-09-28 22:53:49 +0800

    list

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,7 @@
 const { requsetForGet, requsetForGetUs } = require('../../utils/request')
 const { getAna } = require('../../utils/ana')
+const { setStorage,getStorage} = require('../../utils/weixin')
+const { getDate, isExpire} = require('../../utils/util')
 Page({
   data: {
     title: 'urlDemo',
@@ -16,6 +18,22 @@ Page({
   //生命周期函数--监听页面加载
   onLoad: function () {
     this.initus()
+    let vm =this
+    getStorage('mlist').then(res=>{
+      if (isExpire(res.data.time)) {
+        console.info('cache mlist')
+        vm.setData({
+          movieList: res.data.mlist
+        })
+      }else {
+        vm.initMovies()
+      }
+    }).catch(()=>{
+      vm.initMovies()
+    })
+
+  },
+  initMovies () {
     let allPromises = this.data.movieList.map(m => {
       return requsetForGet(m.url, { count: 10 }).then((res) => {
         m.title = res.data.title
@@ -27,22 +45,40 @@ Page({
       this.setData({
         movieList: res
       })
-      console.info(res)
+      setStorage('mlist', { time: getDate(), mlist: res})
+      console.info('add cache mlist')
     })
   },
   initus () {
     console.log('init us')
     let vm = this
-    requsetForGetUs('https://api.douban.com/v2/movie/us_box').then((res)=>{
-        console.log(res)
-        let us = {}
-        us.url = 'https://api.douban.com/v2/movie/us_box'
-        us.title = res.data.title
-        us.subjects = res.data.subjects
-        us.date = res.data.date
+    getStorage('us').then(res=>{
+      if (isExpire(res.data.time)) {
+        console.info('cache us')
         vm.setData({
-          item: us
+          item: res.data.us
         })
+      }else {
+        vm.initUsCache()
+      }
+    }).catch(()=>{
+      vm.initUsCache()
+    })
+  },
+  initUsCache(){
+    let vm = this
+    requsetForGetUs('https://api.douban.com/v2/movie/us_box').then((res) => {
+      console.log(res)
+      let us = {}
+      us.url = 'https://api.douban.com/v2/movie/us_box'
+      us.title = res.data.title
+      us.subjects = res.data.subjects
+      us.date = res.data.date
+      vm.setData({
+        item: us
+      })
+      setStorage('us', { time: getDate(), us: us})
+      console.info('add cache us')
     })
   },
   more(event) {
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,14 +1,68 @@
 const { requsetForGet } = require('../../utils/request')
 const { getAna } = require('../../utils/ana')
-
+const { getDate, isExpire } = require('../../utils/util')
+const { setStorage, getStorage } = require('../../utils/weixin')
 Page({
   data: {
     ana: getAna(),
-    pageNum: 0
+    pageNum: 0,
+    subjects: [],
+    loading: false,
+    down: false,
+    total: 0,
+    url: '',
+    height: 0
   },
   //生命周期函数--监听页面加载
   onLoad: function (options) {
-    console.info('====================')
-    console.info(options.type)
+    console.info('onLoad')
+    let type = options.type
+    let url = `https://api.douban.com/v2/movie/${type}`
+    this.data.url = url
+    let vm = this
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res.windowHeight)
+        vm.setData({
+          height: res.windowHeight
+        })
+      }
+    })
+    this.init()
+  },
+  init() {
+    console.info('==============当前页:' + this.data.pageNum + '总页数:' + this.data.total)
+    let vm = this
+    let params = { start: this.data.pageNum * 20, count: 20 }
+    requsetForGet(this.data.url, params).then(res => {
+      if (vm.data.total == 0) {
+        vm.data.total = Math.ceil(res.data.total / 20)
+      }
+      vm.data.pageNum += 1
+      let temp = vm.data.subjects
+      Array.prototype.push.apply(temp, res.data.subjects)
+      vm.setData({
+        subjects: temp
+      })
+      console.info(vm.data.subjects)
+      vm.setData({
+        loading: false
+      })
+    })
+
+  },
+  onReachBottom() {
+    console.info('一共' + this.data.total + '页,第' + (this.data.pageNum) + '页')
+    if (this.data.pageNum < this.data.total) {
+      console.info('mmmmmmmmmmmm')
+      this.init()
+      this.setData({
+        loading: true
+      })
+    } else {
+      this.setData({
+        down: true
+      })
+    }
   }
 })
